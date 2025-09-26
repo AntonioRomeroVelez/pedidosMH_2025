760018832f85cfc3441736eb0447602f6b171b2f
Antonio Romero <tonio93romero@gmail.com>
2025-09-26 14:26:19 -0500

    nuevos

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "alertifyjs/build/css/alertify.min.css";
 import "alertifyjs/build/css/themes/default.min.css"; // Puedes cambiar el tema si quieres
+
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
-app.use(createPinia());
 app.use(router);
+
 app.mount("#app");
+ñ;
